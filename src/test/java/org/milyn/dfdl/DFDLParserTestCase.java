f425632018f2047adac8f99bc92eeb3dfddca550
claude.mamo <claude.mamo@gmail.com>
2020-02-18 18:04:40 +0100

    Added test coverage

--- src/test/java/org/milyn/dfdl/DFDLParserTestCase.java
+++ src/test/java/org/milyn/dfdl/DFDLParserTestCase.java
@@ -4,21 +4,20 @@ import org.apache.daffodil.japi.Daffodil;
 import org.apache.daffodil.japi.DataProcessor;
 import org.apache.daffodil.japi.ProcessorFactory;
 import org.junit.jupiter.api.Test;
+import org.milyn.Smooks;
 import org.milyn.container.ExecutionContext;
 import org.milyn.container.MockApplicationContext;
-import org.milyn.container.MockExecutionContext;
-import org.milyn.delivery.dom.DOMBuilder;
+import org.milyn.delivery.sax.SAXHandler;
 import org.milyn.dfdl.delivery.AbstractDFDLContentHandlerFactory;
 import org.milyn.io.StreamUtils;
 import org.milyn.namespace.NamespaceDeclarationStack;
-import org.milyn.xml.XmlUtil;
-import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 
+import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DFDLParserTestCase {
 
@@ -31,22 +30,50 @@ public class DFDLParserTestCase {
         Map<String, DataProcessor> schemas = new HashMap<>();
         schemas.put("classpath:/csv.dfdl.xsd", dataProcessor);
 
-        ExecutionContext executionContext = new MockExecutionContext();
+        ExecutionContext executionContext = new Smooks().createExecutionContext();
         executionContext.setAttribute(NamespaceDeclarationStack.class, new NamespaceDeclarationStack());
 
-        DOMBuilder domBuilder = new DOMBuilder(executionContext);
+        StringWriter stringWriter = new StringWriter();
+        SAXHandler saxHandler = new SAXHandler(executionContext, stringWriter);
 
         DFDLParser dfdlParser = new DFDLParser();
         dfdlParser.setSchemaURI("classpath:/csv.dfdl.xsd");
         dfdlParser.setApplicationContext(new MockApplicationContext());
         dfdlParser.setIndent(true);
-        dfdlParser.setContentHandler(domBuilder);
+        dfdlParser.setContentHandler(saxHandler);
         dfdlParser.getApplicationContext().setAttribute(AbstractDFDLContentHandlerFactory.class, schemas);
 
         dfdlParser.initialize();
         dfdlParser.parse(new InputSource(getClass().getResourceAsStream("/simpleCSV.csv")));
 
-        Document document = domBuilder.getDocument();
-        assertTrue(StreamUtils.compareCharStreams(StreamUtils.readStreamAsString(getClass().getResourceAsStream("/simpleCSV.xml")), XmlUtil.serialize(document, true)));
+        assertEquals(StreamUtils.readStreamAsString(getClass().getResourceAsStream("/simpleCSV.xml")), stringWriter.toString());
+    }
+
+    @Test
+    public void testParseGivenIndentIsFalse() throws Exception {
+        org.apache.daffodil.japi.Compiler compiler = Daffodil.compiler();
+        ProcessorFactory processorFactory = compiler.compileSource(getClass().getResource("/csv.dfdl.xsd").toURI());
+        DataProcessor dataProcessor = processorFactory.onPath("/");
+
+        Map<String, DataProcessor> schemas = new HashMap<>();
+        schemas.put("classpath:/csv.dfdl.xsd", dataProcessor);
+
+        ExecutionContext executionContext = new Smooks().createExecutionContext();
+        executionContext.setAttribute(NamespaceDeclarationStack.class, new NamespaceDeclarationStack());
+
+        StringWriter stringWriter = new StringWriter();
+        SAXHandler saxHandler = new SAXHandler(executionContext, stringWriter);
+
+        DFDLParser dfdlParser = new DFDLParser();
+        dfdlParser.setSchemaURI("classpath:/csv.dfdl.xsd");
+        dfdlParser.setApplicationContext(new MockApplicationContext());
+        dfdlParser.setIndent(false);
+        dfdlParser.setContentHandler(saxHandler);
+        dfdlParser.getApplicationContext().setAttribute(AbstractDFDLContentHandlerFactory.class, schemas);
+
+        dfdlParser.initialize();
+        dfdlParser.parse(new InputSource(getClass().getResourceAsStream("/simpleCSV.csv")));
+
+        assertEquals(StreamUtils.trimLines(StreamUtils.readStreamAsString(getClass().getResourceAsStream("/simpleCSV.xml"))), stringWriter.toString());
     }
 }
