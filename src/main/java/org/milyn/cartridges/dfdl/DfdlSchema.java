562b19b1b62a708e52b661c387a00ff315881e0f
claude.mamo <claude.mamo@gmail.com>
2020-03-11 01:49:30 +0100

    Refactoring

---
+++ src/main/java/org/milyn/cartridges/dfdl/DfdlSchema.java
@@ -0,0 +1,62 @@
+package org.milyn.cartridges.dfdl;
+
+import org.apache.daffodil.japi.Daffodil;
+import org.apache.daffodil.japi.DataProcessor;
+import org.apache.daffodil.japi.Diagnostic;
+import org.apache.daffodil.japi.ProcessorFactory;
+import scala.Predef;
+import scala.collection.JavaConverters;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+public class DfdlSchema {
+
+    private final URI uri;
+    private final Map<String, String> variables;
+    private final boolean validateSchemas;
+
+    public DfdlSchema(final URI uri, final Map<String, String> variables, boolean validateSchemas) {
+        this.uri = uri;
+        this.variables = variables;
+        this.validateSchemas = validateSchemas;
+    }
+
+    public URI getUri() {
+        return uri;
+    }
+
+    public Map<String, String> getVariables() {
+        return variables;
+    }
+
+    public boolean isValidateSchemas() {
+        return validateSchemas;
+    }
+
+    public String getName() {
+        return uri + ":" + validateSchemas + ":" + variables.toString();
+    }
+
+    public DataProcessor compile() throws Throwable {
+        final org.apache.daffodil.japi.Compiler compiler = Daffodil.compiler();
+        compiler.setValidateDFDLSchemas(validateSchemas);
+
+        final ProcessorFactory processorFactory = compiler.compileSource(uri);
+        if (processorFactory.isError()) {
+            final List<Diagnostic> diagnostics = processorFactory.getDiagnostics();
+            throw diagnostics.get(0).getSomeCause();
+        }
+        final DataProcessor dataProcessor = processorFactory.onPath("/");
+        if (dataProcessor.isError()) {
+            final List<Diagnostic> diagnostics = dataProcessor.getDiagnostics();
+            for (Diagnostic diagnostic : diagnostics) {
+                throw diagnostic.getSomeCause();
+            }
+        }
+
+        dataProcessor.setExternalVariables(JavaConverters.mapAsScalaMapConverter(variables).asScala().toMap(Predef.$conforms()));
+        return dataProcessor;
+    }
+}
--- src/main/java/org/milyn/cartridges/dfdl/delivery/DfdlParserContentHandlerFactory.java
+++ src/main/java/org/milyn/cartridges/dfdl/delivery/DfdlParserContentHandlerFactory.java
@@ -1,6 +1,7 @@
 package org.milyn.cartridges.dfdl.delivery;
 
 import org.apache.daffodil.japi.DataProcessor;
+import org.milyn.cartridges.dfdl.DfdlSchema;
 import org.milyn.cdr.SmooksConfigurationException;
 import org.milyn.cdr.SmooksResourceConfiguration;
 import org.milyn.delivery.ContentHandler;
@@ -9,8 +10,8 @@ import org.milyn.delivery.annotation.Resource;
 @Resource(type = "dfdl-parser")
 public class DfdlParserContentHandlerFactory extends AbstractDfdlContentHandlerFactory {
     @Override
-    public ContentHandler doCreate(final SmooksResourceConfiguration smooksResourceConfiguration, final String dataProcessorName, final DataProcessor dataProcessor) throws SmooksConfigurationException {
-        smooksResourceConfiguration.setParameter("dataProcessorName", dataProcessorName);
+    public ContentHandler doCreate(final SmooksResourceConfiguration smooksResourceConfiguration, final DfdlSchema dfdlSchema, final DataProcessor dataProcessor) throws SmooksConfigurationException {
+        smooksResourceConfiguration.setParameter("dataProcessorName", dfdlSchema.getName());
         smooksResourceConfiguration.setResource("org.milyn.cartridges.dfdl.DfdlParser");
         return null;
     }
