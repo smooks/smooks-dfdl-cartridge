smooks/smooks-dfdl-cartridge

commit c83aba6beec32a983f51f263d3fcee0f317b3058

build(deps): bump daffodil-japi_2.12 from 3.0.0 to 3.2.0 (#79)

src/main/java/org/smooks/cartridges/dfdl/parser/DfdlParser.java
@@ -64,6 +64,8 @@ import org.smooks.engine.lifecycle.PostConstructLifecyclePhase;
 import org.smooks.engine.lookup.LifecycleManagerLookup;
 import org.xml.sax.*;
 import org.xml.sax.helpers.AttributesImpl;
+import scala.Function0;
+import scala.Option;
 import scala.xml.NamespaceBinding;
 import scala.xml.TopScope$;
 
@@ -291,8 +293,8 @@ public class DfdlParser implements SmooksXMLReader {
                 }
 
                 private String getQName(final DIElement diElement) {
-                    final String prefix = diElement.erd().namedQName().prefixOrNull();
-                    return (prefix == null || prefix.equals("")) ? "" : prefix + ":" + diElement.erd().name();
+                    final Option<String> prefix = diElement.erd().namedQName().prefix();
+                    return (prefix.isEmpty() || prefix.get().equals("")) ? "" : prefix.get() + ":" + diElement.erd().name();
                 }
             });
             if (parseResult.isError()) {

pom.xml
@@ -203,7 +203,7 @@
         <dependency>
             <groupId>org.apache.daffodil</groupId>
             <artifactId>daffodil-japi_2.12</artifactId>
-            <version>3.0.0</version>
+            <version>3.2.0</version>
             <exclusions>
                 <exclusion>
                     <groupId>com.fasterxml.woodstox</groupId>

src/main/java/org/smooks/cartridges/dfdl/DfdlSchema.java
@@ -42,7 +42,11 @@
  */
 package org.smooks.cartridges.dfdl;
 
-import org.apache.daffodil.japi.*;
+import org.apache.daffodil.japi.Daffodil;
+import org.apache.daffodil.japi.DataProcessor;
+import org.apache.daffodil.japi.Diagnostic;
+import org.apache.daffodil.japi.ProcessorFactory;
+import org.apache.daffodil.japi.ValidationMode;
 import org.apache.daffodil.japi.debugger.TraceDebuggerRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -118,14 +122,14 @@ public class DfdlSchema {
         }
 
         if (debugging) {
-            dataProcessor = dataProcessor.withDebugger(new TraceDebuggerRunner()).withDebugging(true);
+            dataProcessor = dataProcessor.withDebuggerRunner(new TraceDebuggerRunner()).withDebugging(true);
         }
 
         return dataProcessor.withValidationMode(validationMode).withExternalVariables(new HashMap<>(variables));
     }
 
     protected DataProcessor compileSource() throws Throwable {
-        final org.apache.daffodil.japi.Compiler compiler = Daffodil.compiler();
+        org.apache.daffodil.japi.Compiler compiler = Daffodil.compiler();
         if (distinguishedRootNode != null) {
             compiler.setDistinguishedRootNode(distinguishedRootNode.substring(distinguishedRootNode.indexOf("}") + 1), distinguishedRootNode.substring(distinguishedRootNode.indexOf("{") + 1, distinguishedRootNode.indexOf("}")));
         }

commit b98c8cd2c5020717257814d1a3b616816c719086

fix: solve NullPointerException happening in smooks-edifact-cartridge caused by https://github.com/smooks/smooks-dfdl-cartridge/pull/79

src/main/java/org/smooks/cartridges/dfdl/parser/ContentHandlerInfosetOutputter.java
@@ -1,0 +1,197 @@
+/*-
+ * ========================LICENSE_START=================================
+ * Smooks DFDL Cartridge
+ * %%
+ * Copyright (C) 2020 - 2021 Smooks
+ * %%
+ * Licensed under the terms of the Apache License Version 2.0, or
+ * the GNU Lesser General Public License version 3.0 or later.
+ * 
+ * SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-or-later
+ * 
+ * ======================================================================
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * ======================================================================
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * =========================LICENSE_END==================================
+ */
+package org.smooks.cartridges.dfdl.parser;
+
+import org.apache.daffodil.infoset.DIArray;
+import org.apache.daffodil.infoset.DIComplex;
+import org.apache.daffodil.infoset.DIElement;
+import org.apache.daffodil.infoset.DISimple;
+import org.apache.daffodil.japi.infoset.InfosetOutputter;
+import org.smooks.api.SmooksException;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
+import scala.Option;
+import scala.xml.NamespaceBinding;
+import scala.xml.TopScope$;
+
+import javax.xml.XMLConstants;
+
+import static javax.xml.XMLConstants.NULL_NS_URI;
+import static javax.xml.XMLConstants.W3C_XML_SCHEMA_INSTANCE_NS_URI;
+
+class ContentHandlerInfosetOutputter extends InfosetOutputter {
+    protected static final char[] INDENT = "\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t".toCharArray();
+
+    protected final ContentHandler contentHandler;
+    protected final boolean indent;
+    protected int elementLevel = 0;
+
+    ContentHandlerInfosetOutputter(final ContentHandler contentHandler, boolean indent) {
+        this.contentHandler = contentHandler;
+        this.indent = indent;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public boolean startDocument() {
+        try {
+            contentHandler.startDocument();
+        } catch (SAXException e) {
+            throw new SmooksException(e.getMessage(), e);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean endDocument() {
+        try {
+            contentHandler.endDocument();
+        } catch (SAXException e) {
+            throw new SmooksException(e.getMessage(), e);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean startSimple(final DISimple diSimple) {
+        try {
+            final AttributesImpl attributes = createAttributes(diSimple);
+            if (isNilled(diSimple)) {
+                attributes.addAttribute(W3C_XML_SCHEMA_INSTANCE_NS_URI, "nil", "xsi:nil", "NMTOKEN", "true");
+            }
+            indent(elementLevel);
+            contentHandler.startElement(getNamespaceUri(diSimple), diSimple.erd().name(), getQName(diSimple), attributes);
+            if (!isNilled(diSimple) && diSimple.hasValue()) {
+                contentHandler.characters(diSimple.dataValueAsString().toCharArray(), 0, diSimple.dataValueAsString().length());
+            }
+        } catch (Exception e) {
+            throw new SmooksException(e.getMessage(), e);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean endSimple(final DISimple diSimple) {
+        try {
+            contentHandler.endElement(getNamespaceUri(diSimple), diSimple.erd().name(), getQName(diSimple));
+        } catch (Exception e) {
+            throw new SmooksException(e.getMessage(), e);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean startComplex(final DIComplex diComplex) {
+        try {
+            indent(elementLevel);
+            final String nsUri = getNamespaceUri(diComplex);
+            contentHandler.startElement(nsUri, diComplex.erd().name(), getQName(diComplex), createAttributes(diComplex));
+            elementLevel++;
+            if (diComplex.isEmpty()) {
+                elementLevel--;
+                contentHandler.endElement(nsUri, diComplex.erd().name(), getQName(diComplex));
+            }
+        } catch (SAXException e) {
+            throw new SmooksException(e.getMessage(), e);
+        }
+        return true;
+    }
+
+    protected AttributesImpl createAttributes(final DIElement diElement) {
+        final AttributesImpl attributes = new AttributesImpl();
+        if (diElement.erd().namedQName().prefix().isDefined()) {
+            final NamespaceBinding nsbStart = diElement.erd().minimizedScope();
+            final NamespaceBinding nsbEnd = diElement.isRoot() ? TopScope$.MODULE$ : diElement.diParent().erd().minimizedScope();
+            if (nsbStart != nsbEnd) {
+                NamespaceBinding namespaceBinding = nsbStart;
+                while (namespaceBinding != TopScope$.MODULE$) {
+                    attributes.addAttribute(XMLConstants.XMLNS_ATTRIBUTE_NS_URI, namespaceBinding.prefix(), XMLConstants.XMLNS_ATTRIBUTE + ":" + namespaceBinding.prefix(), "CDATA", namespaceBinding.uri());
+                    namespaceBinding = namespaceBinding.copy$default$3();
+                }
+            }
+        }
+        return attributes;
+    }
+
+    @Override
+    public boolean endComplex(final DIComplex diComplex) {
+        try {
+            elementLevel--;
+            indent(elementLevel);
+            contentHandler.endElement(diComplex.erd().targetNamespace().toString(), diComplex.erd().name(), getQName(diComplex));
+        } catch (SAXException e) {
+            throw new SmooksException(e.getMessage(), e);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean startArray(final DIArray diArray) {
+        return true;
+    }
+
+    @Override
+    public boolean endArray(final DIArray diArray) {
+        return true;
+    }
+
+    protected String getQName(final DIElement diElement) {
+        final Option<String> prefix = diElement.erd().namedQName().prefix();
+        return (prefix.isEmpty() || prefix.get().equals("")) ? "" : prefix.get() + ":" + diElement.erd().name();
+    }
+
+    protected void indent(final int elementLevel) throws SAXException {
+        if (indent) {
+            contentHandler.characters(INDENT, 0, elementLevel + 1);
+        }
+    }
+
+    protected String getNamespaceUri(final DIElement diElement) {
+        return diElement.erd().namedQName().namespace().isNoNamespace() ? NULL_NS_URI : diElement.erd().namedQName().namespace().toString();
+    }
+}

src/main/java/org/smooks/cartridges/dfdl/parser/DfdlParser.java
@@ -42,14 +42,9 @@
  */
 package org.smooks.cartridges.dfdl.parser;
 
-import org.apache.daffodil.infoset.DIArray;
-import org.apache.daffodil.infoset.DIComplex;
-import org.apache.daffodil.infoset.DIElement;
-import org.apache.daffodil.infoset.DISimple;
 import org.apache.daffodil.japi.DataProcessor;
 import org.apache.daffodil.japi.Diagnostic;
 import org.apache.daffodil.japi.ParseResult;
-import org.apache.daffodil.japi.infoset.InfosetOutputter;
 import org.apache.daffodil.japi.io.InputSourceDataInputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -62,25 +57,21 @@ import org.smooks.cartridges.dfdl.DataProcessorFactory;
 import org.smooks.engine.injector.Scope;
 import org.smooks.engine.lifecycle.PostConstructLifecyclePhase;
 import org.smooks.engine.lookup.LifecycleManagerLookup;
-import org.xml.sax.*;
-import org.xml.sax.helpers.AttributesImpl;
-import scala.Function0;
-import scala.Option;
-import scala.xml.NamespaceBinding;
-import scala.xml.TopScope$;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.DTDHandler;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXNotRecognizedException;
+import org.xml.sax.SAXNotSupportedException;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.xml.XMLConstants;
-
-import static javax.xml.XMLConstants.NULL_NS_URI;
-import static javax.xml.XMLConstants.W3C_XML_SCHEMA_INSTANCE_NS_URI;
 
 public class DfdlParser implements SmooksXMLReader {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DfdlParser.class);
-    private static final char[] INDENT = "\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t".toCharArray();
 
     protected DataProcessor dataProcessor;
 
@@ -182,121 +173,7 @@ public class DfdlParser implements SmooksXMLReader {
         final InputSourceDataInputStream inputSourceDataInputStream = new InputSourceDataInputStream(input.getByteStream());
         ParseResult parseResult = null;
         while (parseResult == null || !parseResult.location().isAtEnd()) {
-            parseResult = dataProcessor.parse(inputSourceDataInputStream, new InfosetOutputter() {
-                private int elementLevel = 0;
-
-                @Override
-                public void reset() {
-
-                }
-
-                @Override
-                public boolean startDocument() {
-                    try {
-                        contentHandler.startDocument();
-                    } catch (SAXException e) {
-                        throw new SmooksException(e.getMessage(), e);
-                    }
-                    return true;
-                }
-
-                @Override
-                public boolean endDocument() {
-                    try {
-                        contentHandler.endDocument();
-                    } catch (SAXException e) {
-                        throw new SmooksException(e.getMessage(), e);
-                    }
-                    return true;
-                }
-
-                @Override
-                public boolean startSimple(final DISimple diSimple) {
-                    try {
-                        final AttributesImpl attributes = createAttributes(diSimple);
-                        if (isNilled(diSimple)) {
-                            attributes.addAttribute(W3C_XML_SCHEMA_INSTANCE_NS_URI, "nil", "xsi:nil", "NMTOKEN", "true");
-                        }
-                        indent(elementLevel);
-                        contentHandler.startElement(getNamespaceUri(diSimple), diSimple.erd().name(), getQName(diSimple), attributes);
-                        if (!isNilled(diSimple) && diSimple.hasValue()) {
-                            contentHandler.characters(diSimple.dataValueAsString().toCharArray(), 0, diSimple.dataValueAsString().length());
-                        }
-                    } catch (Exception e) {
-                        throw new SmooksException(e.getMessage(), e);
-                    }
-                    return true;
-                }
-
-                @Override
-                public boolean endSimple(final DISimple diSimple) {
-                    try {
-                        contentHandler.endElement(getNamespaceUri(diSimple), diSimple.erd().name(), getQName(diSimple));
-                    } catch (Exception e) {
-                        throw new SmooksException(e.getMessage(), e);
-                    }
-                    return true;
-                }
-
-                @Override
-                public boolean startComplex(final DIComplex diComplex) {
-                    try {
-                        indent(elementLevel);
-                        final String nsUri = getNamespaceUri(diComplex);
-                        contentHandler.startElement(nsUri, diComplex.erd().name(), getQName(diComplex), createAttributes(diComplex));
-                        elementLevel++;
-                        if (diComplex.isEmpty()) {
-                            elementLevel--;
-                            contentHandler.endElement(nsUri, diComplex.erd().name(), getQName(diComplex));
-                        }
-                    } catch (SAXException e) {
-                        throw new SmooksException(e.getMessage(), e);
-                    }
-                    return true;
-                }
-
-                private AttributesImpl createAttributes(final DIElement diElement) {
-                    final NamespaceBinding nsbStart = diElement.erd().minimizedScope();
-                    final NamespaceBinding nsbEnd = diElement.isRoot() ? TopScope$.MODULE$ : diElement.diParent().erd().minimizedScope();
-                    final AttributesImpl attributes = new AttributesImpl();
-                    if (nsbStart != nsbEnd) {
-                        NamespaceBinding namespaceBinding = nsbStart;
-                        while (namespaceBinding != TopScope$.MODULE$) {
-                            attributes.addAttribute(XMLConstants.XMLNS_ATTRIBUTE_NS_URI, namespaceBinding.prefix(), XMLConstants.XMLNS_ATTRIBUTE + ":" + namespaceBinding.prefix(), "CDATA", namespaceBinding.uri());
-                            namespaceBinding = namespaceBinding.copy$default$3();
-                        }
-                    }
-
-                    return attributes;
-                }
-
-                @Override
-                public boolean endComplex(final DIComplex diComplex) {
-                    try {
-                        elementLevel--;
-                        indent(elementLevel);
-                        contentHandler.endElement(diComplex.erd().targetNamespace().toString(), diComplex.erd().name(), getQName(diComplex));
-                    } catch (SAXException e) {
-                        throw new SmooksException(e.getMessage(), e);
-                    }
-                    return true;
-                }
-
-                @Override
-                public boolean startArray(final DIArray diArray) {
-                    return true;
-                }
-
-                @Override
-                public boolean endArray(final DIArray diArray) {
-                    return true;
-                }
-
-                private String getQName(final DIElement diElement) {
-                    final Option<String> prefix = diElement.erd().namedQName().prefix();
-                    return (prefix.isEmpty() || prefix.get().equals("")) ? "" : prefix.get() + ":" + diElement.erd().name();
-                }
-            });
+            parseResult = dataProcessor.parse(inputSourceDataInputStream, new ContentHandlerInfosetOutputter(contentHandler, indent));
             if (parseResult.isError()) {
                 for (Diagnostic diagnostic : parseResult.getDiagnostics()) {
                     if (diagnostic.isError()) {
@@ -315,12 +192,6 @@ public class DfdlParser implements SmooksXMLReader {
     @Override
     public void parse(final String systemId) {
 
-    }
-
-    private void indent(final int elementLevel) throws SAXException {
-        if (indent) {
-            contentHandler.characters(INDENT, 0, elementLevel + 1);
-        }
     }
 
     public ApplicationContext getApplicationContext() {
@@ -357,9 +228,5 @@ public class DfdlParser implements SmooksXMLReader {
 
     public void setSchemaUri(final String schemaUri) {
         this.schemaUri = schemaUri;
-    }
-    
-    private String getNamespaceUri(final DIElement diElement) {
-        return diElement.erd().namedQName().namespace().isNoNamespace() ? NULL_NS_URI : diElement.erd().namedQName().namespace().toString();
     }
 }

src/test/java/org/smooks/cartridges/dfdl/parser/ContentHandlerInfosetOutputterTestCase.java
@@ -1,0 +1,307 @@
+/*-
+ * ========================LICENSE_START=================================
+ * Smooks DFDL Cartridge
+ * %%
+ * Copyright (C) 2020 - 2021 Smooks
+ * %%
+ * Licensed under the terms of the Apache License Version 2.0, or
+ * the GNU Lesser General Public License version 3.0 or later.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-or-later
+ *
+ * ======================================================================
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ======================================================================
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * =========================LICENSE_END==================================
+ */
+package org.smooks.cartridges.dfdl.parser;
+
+import org.apache.daffodil.infoset.ContentLengthState;
+import org.apache.daffodil.infoset.DIElement;
+import org.apache.daffodil.infoset.DINode;
+import org.apache.daffodil.infoset.InfosetComplexElement;
+import org.apache.daffodil.infoset.ValueLengthState;
+import org.apache.daffodil.processors.ElementRuntimeData;
+import org.apache.daffodil.xml.GlobalQName;
+import org.apache.daffodil.xml.NS;
+import org.junit.jupiter.api.Test;
+import org.xml.sax.helpers.AttributesImpl;
+import scala.collection.IndexedSeq;
+import scala.collection.immutable.Stream;
+import scala.xml.NamespaceBinding;
+import scala.xml.TopScope$;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ContentHandlerInfosetOutputterTestCase {
+
+    private static class StubDIElement implements DIElement {
+
+        private final ElementRuntimeData erd;
+
+        public StubDIElement(ElementRuntimeData erd) {
+            this.erd = erd;
+        }
+
+        @Override
+        public boolean isRoot() {
+            return true;
+        }
+
+        @Override
+        public ContentLengthState _contentLength() {
+            return null;
+        }
+
+        @Override
+        public void _contentLength_$eq(ContentLengthState x$1) {
+
+        }
+
+        @Override
+        public ValueLengthState _valueLength() {
+            return null;
+        }
+
+        @Override
+        public void _valueLength_$eq(ValueLengthState x$1) {
+
+        }
+
+        @Override
+        public boolean _isNilled() {
+            return false;
+        }
+
+        @Override
+        public void _isNilled_$eq(boolean x$1) {
+
+        }
+
+        @Override
+        public int _validity() {
+            return 0;
+        }
+
+        @Override
+        public void _validity_$eq(int x$1) {
+
+        }
+
+        @Override
+        public ContentLengthState contentLength() {
+            return null;
+        }
+
+        @Override
+        public ValueLengthState valueLength() {
+            return null;
+        }
+
+        @Override
+        public boolean isSimple() {
+            return false;
+        }
+
+        @Override
+        public boolean isComplex() {
+            return false;
+        }
+
+        @Override
+        public boolean isArray() {
+            return false;
+        }
+
+        @Override
+        public int infosetWalkerBlockCount() {
+            return 0;
+        }
+
+        @Override
+        public IndexedSeq<DINode> contents() {
+            return null;
+        }
+
+        @Override
+        public Object maybeLastChild() {
+            return null;
+        }
+
+        @Override
+        public void freeChildIfNoLongerNeeded(int index, boolean doFree) {
+
+        }
+
+        @Override
+        public boolean wouldHaveBeenFreed() {
+            return false;
+        }
+
+        @Override
+        public void wouldHaveBeenFreed_$eq(boolean x$1) {
+
+        }
+
+        @Override
+        public boolean isFinal() {
+            return false;
+        }
+
+        @Override
+        public void isFinal_$eq(boolean x$1) {
+
+        }
+
+        @Override
+        public void requireFinal() {
+
+        }
+
+        @Override
+        public void infosetWalkerBlockCount_$eq(int x$1) {
+
+        }
+
+        @Override
+        public boolean isDefaulted() {
+            return false;
+        }
+
+        @Override
+        public Stream<DINode> children() {
+            return null;
+        }
+
+        @Override
+        public long totalElementCount() {
+            return 0;
+        }
+
+        @Override
+        public ElementRuntimeData erd() {
+            return erd;
+        }
+
+        @Override
+        public String valueStringForDebug() {
+            return null;
+        }
+
+        @Override
+        public boolean _isHidden() {
+            return false;
+        }
+
+        @Override
+        public void _isHidden_$eq(boolean x$1) {
+
+        }
+
+        @Override
+        public InfosetComplexElement _parent() {
+            return null;
+        }
+
+        @Override
+        public void _parent_$eq(InfosetComplexElement x$1) {
+
+        }
+
+        @Override
+        public boolean _isNilledSet() {
+            return false;
+        }
+
+        @Override
+        public void _isNilledSet_$eq(boolean x$1) {
+
+        }
+
+        @Override
+        public Object org$apache$daffodil$infoset$DIElement$$_array() {
+            return null;
+        }
+
+        @Override
+        public void org$apache$daffodil$infoset$DIElement$$_array_$eq(Object x$1) {
+
+        }
+
+        @Override
+        public boolean isNilled() {
+            return false;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+    }
+
+    @Test
+    public void testCreateAttributesGivenUndefinedLocalName() throws URISyntaxException {
+        ContentHandlerInfosetOutputter contentHandlerInfosetOutputter = new ContentHandlerInfosetOutputter(null, ThreadLocalRandom.current().nextBoolean());
+        ElementRuntimeData erd = new ElementRuntimeData(0, null, null, null, null,
+                null, null, null, null, new NamespaceBinding(null, null, TopScope$.MODULE$), null,
+                null, null, null, null,
+                0, 0, null, null, null,
+                ThreadLocalRandom.current().nextBoolean(), ThreadLocalRandom.current().nextBoolean(), ThreadLocalRandom.current().nextBoolean(),
+                ThreadLocalRandom.current().nextBoolean(), new GlobalQName(scala.Option.apply(null), null, new NS(new URI(""))), ThreadLocalRandom.current().nextBoolean(),
+                ThreadLocalRandom.current().nextBoolean(), 0, false, null,
+                null, null, null, null, null,
+                null, null, null, ThreadLocalRandom.current().nextBoolean(),
+                null);
+
+        AttributesImpl attributes = contentHandlerInfosetOutputter.createAttributes(new StubDIElement(erd));
+        assertEquals(0, attributes.getLength());
+    }
+
+    @Test
+    public void testCreateAttributesGivenLocalName() throws URISyntaxException {
+        ContentHandlerInfosetOutputter contentHandlerInfosetOutputter = new ContentHandlerInfosetOutputter(null, ThreadLocalRandom.current().nextBoolean());
+        ElementRuntimeData erd = new ElementRuntimeData(0, null, null, null, null,
+                null, null, null, null, new NamespaceBinding(null, null, TopScope$.MODULE$), null,
+                null, null, null, null,
+                0, 0, null, null, null,
+                ThreadLocalRandom.current().nextBoolean(), ThreadLocalRandom.current().nextBoolean(), ThreadLocalRandom.current().nextBoolean(),
+                ThreadLocalRandom.current().nextBoolean(), new GlobalQName(scala.Option.apply("foo"), null, new NS(new URI(""))), ThreadLocalRandom.current().nextBoolean(),
+                ThreadLocalRandom.current().nextBoolean(), 0, false, null,
+                null, null, null, null, null,
+                null, null, null, ThreadLocalRandom.current().nextBoolean(),
+                null);
+
+        AttributesImpl attributes = contentHandlerInfosetOutputter.createAttributes(new StubDIElement(erd));
+        assertEquals(1, attributes.getLength());
+    }
+}