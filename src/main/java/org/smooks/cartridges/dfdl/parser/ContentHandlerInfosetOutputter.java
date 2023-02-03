2717523138d16ad5e15a50306bc59840784de03c
claudemamo <823038+claudemamo@users.noreply.github.com>
2023-02-03 23:21:23 +0100

    chore: bump daffodil-japi_2.12 tp 3.4.0

--- src/main/java/org/smooks/cartridges/dfdl/parser/ContentHandlerInfosetOutputter.java
+++ src/main/java/org/smooks/cartridges/dfdl/parser/ContentHandlerInfosetOutputter.java
@@ -6,35 +6,35 @@
  * %%
  * Licensed under the terms of the Apache License Version 2.0, or
  * the GNU Lesser General Public License version 3.0 or later.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-or-later
- * 
+ *
  * ======================================================================
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ======================================================================
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -78,27 +78,25 @@ class ContentHandlerInfosetOutputter extends InfosetOutputter {
     }
 
     @Override
-    public boolean startDocument() {
+    public void startDocument() {
         try {
             contentHandler.startDocument();
         } catch (SAXException e) {
             throw new SmooksException(e.getMessage(), e);
         }
-        return true;
     }
 
     @Override
-    public boolean endDocument() {
+    public void endDocument() {
         try {
             contentHandler.endDocument();
         } catch (SAXException e) {
             throw new SmooksException(e.getMessage(), e);
         }
-        return true;
     }
 
     @Override
-    public boolean startSimple(final DISimple diSimple) {
+    public void startSimple(final DISimple diSimple) {
         try {
             final AttributesImpl attributes = createAttributes(diSimple);
             if (isNilled(diSimple)) {
@@ -112,21 +110,19 @@ class ContentHandlerInfosetOutputter extends InfosetOutputter {
         } catch (Exception e) {
             throw new SmooksException(e.getMessage(), e);
         }
-        return true;
     }
 
     @Override
-    public boolean endSimple(final DISimple diSimple) {
+    public void endSimple(final DISimple diSimple) {
         try {
             contentHandler.endElement(getNamespaceUri(diSimple), diSimple.erd().name(), getQName(diSimple));
         } catch (Exception e) {
             throw new SmooksException(e.getMessage(), e);
         }
-        return true;
     }
 
     @Override
-    public boolean startComplex(final DIComplex diComplex) {
+    public void startComplex(final DIComplex diComplex) {
         try {
             indent(elementLevel);
             final String nsUri = getNamespaceUri(diComplex);
@@ -139,7 +135,6 @@ class ContentHandlerInfosetOutputter extends InfosetOutputter {
         } catch (SAXException e) {
             throw new SmooksException(e.getMessage(), e);
         }
-        return true;
     }
 
     protected AttributesImpl createAttributes(final DIElement diElement) {
@@ -159,7 +154,7 @@ class ContentHandlerInfosetOutputter extends InfosetOutputter {
     }
 
     @Override
-    public boolean endComplex(final DIComplex diComplex) {
+    public void endComplex(final DIComplex diComplex) {
         try {
             elementLevel--;
             indent(elementLevel);
@@ -167,17 +162,14 @@ class ContentHandlerInfosetOutputter extends InfosetOutputter {
         } catch (SAXException e) {
             throw new SmooksException(e.getMessage(), e);
         }
-        return true;
     }
 
     @Override
-    public boolean startArray(final DIArray diArray) {
-        return true;
+    public void startArray(final DIArray diArray) {
     }
 
     @Override
-    public boolean endArray(final DIArray diArray) {
-        return true;
+    public void endArray(final DIArray diArray) {
     }
 
     protected String getQName(final DIElement diElement) {
