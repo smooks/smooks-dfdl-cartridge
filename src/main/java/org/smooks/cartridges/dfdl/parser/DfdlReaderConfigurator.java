6e23d09b71da80db31ff07faaa225c85e44509a9
cjmamo <823038+claudemamo@users.noreply.github.com>
2024-03-04 13:10:47 +0100

    fix: solve compilation errors caused by upstream changes

--- src/main/java/org/smooks/cartridges/dfdl/parser/DfdlReaderConfigurator.java
+++ src/main/java/org/smooks/cartridges/dfdl/parser/DfdlReaderConfigurator.java
@@ -134,7 +134,7 @@ public class DfdlReaderConfigurator implements ReaderConfigurator {
             resourceConfig.setParameter(new DefaultParameter<>("variables", variable));
         }
 
-        resourceConfig.setTargetProfile(targetProfile);
+        resourceConfig.setProfile(targetProfile);
 
         return resourceConfigs;
     }
